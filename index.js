835ddfe25a7bf403f9e97057fb5ccdf4e40040a8
Niklas Eicker <git@nikl.me>
2019-07-15 17:07:09 +0200

    basic site done

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ app.use(express.static("static"))
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
+let images = {
+  images: [{
+      src: "assets/images/kitty1.jpeg",
+      title: "Kitty 1"
+    },
+    {
+      src: "assets/images/kitty2.jpeg",
+      title: "Kitty 2"
+    }
+  ]
+}
+
 app.get("/", function (req, res) {
   res.render("home.hbs")
 })
 
-app.get("/contact", function (req, res) {
-  res.render("contact.hbs")
+app.get("/gallery", function (req, res) {
+  res.render("gallery.hbs", images)
 })
 
 app.get("/about", function (req, res) {
